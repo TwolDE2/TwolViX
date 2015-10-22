d1be63a2c600dd52c56d9e3f6f4dcc245e327f8a
TwolDE <awhalley@waitrose.com>
2015-10-22 16:11:00 +0200

    fix traffic light!

--- lib/python/Components/OnlineUpdateCheck.py
+++ lib/python/Components/OnlineUpdateCheck.py
@@ -1,7 +1,7 @@
 from boxbranding import getImageVersion, getImageBuild, getImageDistro, getMachineBrand, getMachineName, getMachineBuild, getImageType
 
 from time import time
-
+from boxbranding import getImageVersion
 
 from enigma import eTimer
 
@@ -27,7 +27,7 @@ class FeedsStatusCheck:
 
 	def getFeedSatus(self):
 		status = '1'
-		trafficLight == 'stable'
+		trafficLight = 'stable'
 		if getImageType() != 'developer':
 			status = '0'
 			config.softwareupdate.updateisunstable.setValue(status)
