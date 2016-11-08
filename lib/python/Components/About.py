a29944710bae17b5a6cd3ee6f5496cd5e96f1702
TwolDE <awhalley@waitrose.com>
2016-11-08 09:15:51 +0100

    hd51 about rework to ViX solution

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -45,37 +45,45 @@ def getChipSetString():
 
 def getCPUSpeedString():
 	cpu_speed = 0
-	if getMachineBuild() in ('vusolo4k'):
-		try: # Solo4K
+	try:
+		file = open('/proc/cpuinfo', 'r')
+		lines = file.readlines()
+		file.close()
+		for x in lines:
+			splitted = x.split(': ')
+			if len(splitted) > 1:
+				splitted[1] = splitted[1].replace('\n','')
+				if splitted[0].startswith("cpu MHz"):
+					cpu_speed = float(splitted[1].split(' ')[0])
+					break
+	except IOError:
+		print "[About] getCPUSpeedString, /proc/cpuinfo not available"
+
+	if cpu_speed == 0:
+		if getMachineBuild() in ('hd51','hd52'):
+			try:
+				import binascii
+				f = open('/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency', 'rb')
+				clockfrequency = f.read()
+				f.close()
+				cpu_speed = round(int(binascii.hexlify(clockfrequency), 16)/1000000,1)
+			except:
+				return "1,7 GHz"
+		else:
+			try: # Solo4K
 				file = open('/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq', 'r')
 				cpu_speed = float(file.read()) / 1000
 				file.close()
 			except IOError:
-				print "[About] getCPUSpeedString, /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq
-	elif getMachineBuild() in ('hd51','hd52'):
-		try:
-			import binascii
-			f = open('/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency', 'rb')
-			clockfrequency = f.read()
-			f.close()
-			return "%s MHz" % str(round(int(binascii.hexlify(clockfrequency), 16)/1000000,1))
-		except:
-			return "1,7 GHz"
-	else:
-		try:
-			file = open('/proc/cpuinfo', 'r')
-			lines = file.readlines()
-			file.close()
-			for x in lines:
-				splitted = x.split(': ')
-				if len(splitted) > 1:
-					splitted[1] = splitted[1].replace('\n','')
-					if splitted[0].startswith("cpu MHz"):
-						cpu_speed = float(splitted[1].split(' ')[0])
-			return cpu_speed
-		except IOError:
-			print "[About] getCPUSpeedString, /proc/cpuinfo not available"
+				print "[About] getCPUSpeedString, /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq not available"
 
+	if cpu_speed > 0:
+		if cpu_speed >= 1000:
+			cpu_speed = "%s GHz" % str(round(cpu_speed/1000,1))
+		else:
+			cpu_speed = "%s MHz" % str(round(cpu_speed,1))
+		return cpu_speed
+	return _("unavailable")
 
 def getCPUString():
 	if getMachineBuild() in ('vusolo4k', 'hd51', 'hd52'):
