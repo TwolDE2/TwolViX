82a1b9ff653fcfe09698af7c1828cd8cf4654514
Tony Whalley <tonywhalley@t-online.de>
2018-02-05 15:38:38 +0100

    FIX -- restore deleted field

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -1,7 +1,7 @@
 from os import path
 
 from enigma import eDVBResourceManager, Misc_Options
-from Tools.Directories import fileExists, fileCheck
+from Tools.Directories import fileExists, fileCheck, pathExists
 from Tools.HardwareInfo import HardwareInfo
 
 from boxbranding import getMachineBuild, getBoxType, getBrandOEM
