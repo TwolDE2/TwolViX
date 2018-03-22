05bc7e208bce0efdb7105d3ce5a40135bdd5c24b
Tony Whalley <awhalley@waitrose.com>
2018-03-22 13:46:46 +0100

    ViX merge master

--- lib/python/Screens/LogManager.py
+++ lib/python/Screens/LogManager.py
@@ -183,6 +183,7 @@ class LogManager(Screen):
 		self["key_green"] = Button(_("View"))
 		self["key_yellow"] = Button(_("Delete"))
 		self["key_blue"] = Button(_("Send"))
+
 		self.onChangedEntry = [ ]
 		self.sentsingle = ""
 		self.selectedFiles = config.logmanager.sentfiles.value
