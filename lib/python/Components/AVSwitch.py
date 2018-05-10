0c249a48fc6d360b36bd87855a06632df3872402
Tony Whalley <awhalley@waitrose.com>
2018-05-10 11:33:21 +0200

    AVSwitch - try GB resolution on boot

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -287,32 +287,31 @@ class AVSwitch:
 			f = open("/proc/stb/video/videomode_50hz", "w")
 			f.write(mode_50)
 			f.close()
+		except IOError:
+			print "[AVSwitch] cannot open /proc/stb/video/videomode_50hz"
+		try:
 			f = open("/proc/stb/video/videomode_60hz", "w")
 			f.write(mode_60)
 			f.close()
 		except IOError:
-			try:
-				# fallback if no possibility to setup 50/60 hz mode
-				f = open("/proc/stb/video/videomode", "w")
-				f.write(mode_50)
-				f.close()
-			except IOError:
-				print "[AVSwitch] setting videomode failed."
+			print "[AVSwitch] cannot open /proc/stb/video/videomode_50hz"
 
 		if SystemInfo["have24hz"]:
 			try:
 				open("/proc/stb/video/videomode_24hz", "w").write(mode_24)
 			except IOError:
-				print "[VideoHardware] cannot open /proc/stb/video/videomode_24hz"
-
+				print "[AVSwitch] cannot open /proc/stb/video/videomode_24hz"
+ 		try:
+			set_mode = modes.get(int(rate[:2]))
+		except: # not support 50Hz, 60Hz for 1080p
+			set_mode = mode_50
+		print "[AVSwitch] set mode is %s" %set_mode
 		try:
-			# use 50Hz mode (if available) for booting
 			f = open("/etc/videomode", "w")
-			f.write(mode_50)
+			f.write(set_mode)
 			f.close()
 		except IOError:
 			print "[AVSwitch] writing initial videomode to /etc/videomode failed."
-
 		map = {"cvbs": 0, "rgb": 1, "svideo": 2, "yuv": 3}
 		self.setColorFormat(map[config.av.colorformat.value])
 
