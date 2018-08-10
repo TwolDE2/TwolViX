667d8d61869b376be99b6d884f9feee416e7b480
Tony Whalley <awhalley@waitrose.com>
2018-08-10 17:21:17 +0200

    Lcd.py cleanup

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -504,10 +504,6 @@ def InitLcd():
 		else:
 			config.lcd.showoutputresolution = ConfigNothing()
 
-		if fileExists("/proc/stb/power/vfd"):
-			config.lcd.power = ConfigSelection([("0", _("off")), ("1", _("on"))], "1")
-			config.lcd.power.addNotifier(setLCDpower);
-
 	else:
 		def doNothing():
 			pass
