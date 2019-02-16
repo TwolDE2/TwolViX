779f29e3ec29b3594d2170c3c730bfaa738e48ab
Tony Whalley <awhalley@waitrose.com>
2019-02-16 13:25:23 +0100

    H9SDswap add usb

--- lib/python/Screens/H9SDswap.py
+++ lib/python/Screens/H9SDswap.py
@@ -28,30 +28,29 @@ class H9SDswap(Screen):
 		<widget source="key_green" render="Label" position="230,200" size="150,30" noWrap="1" zPosition="1" valign="center" font="Regular; 20" halign="left" backgroundColor="#00000000" foregroundColor="#00ffffff" />
 		<widget source="key_yellow" render="Label" position="430,200" size="150,30" noWrap="1" zPosition="1" valign="center" font="Regular; 20" halign="left" backgroundColor="#00000000" foregroundColor="#00ffffff" />
 		<widget source="key_blue" render="Label" position="630,200" size="150,30" noWrap="1" zPosition="1" valign="center" font="Regular; 20" halign="left" backgroundColor="#00000000" foregroundColor="#00ffffff" />
-		<eLabel position="20,200" size="6,40" backgroundColor="#00e61700" /> <!-- Should be a pixmap -->
-		<eLabel position="190,200" size="6,40" backgroundColor="#0061e500" /> <!-- Should be a pixmap -->
-		<eLabel position="407,209" size="6,40" backgroundColor="#00e5b243" />
+		<ePixmap pixmap="skin_default/buttons/red.png" position="30,200" size="40,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/green.png" position="230,200" size="40,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/yellow.png" position="430,200" size="40,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/blue.png" position="630,200" size="40,40" alphatest="on" />
 	</screen>
 	"""
 
 	def __init__(self, session, *args):
 		Screen.__init__(self, session)
 		self.skinName = "H9SDswap"
-		screentitle = _("H9 switch Nand and SDcard")
+		screentitle = _("H9 switch Nand to SDcard/USB")
 		self["key_red"] = StaticText(_("Cancel"))
 		self["key_green"] = StaticText(_("SwaptoNand"))
 		self["key_yellow"] = StaticText(_("SwaptoSD"))
-		self["key_blue"] = StaticText(_("Reboot"))
+		self["key_blue"] = StaticText(_("SwaptoUSB"))
 		self.title = screentitle
 		self.switchtype = " "
-		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"],
+		self["actions"] = ActionMap(["ColorActions"],
 		{
 			"red": boundFunction(self.close, None),
 			"green": self.SwaptoNand,
 			"yellow": self.SwaptoSD,
-			"blue": self.reboot,
-			"ok": self.reboot,
-			"cancel": boundFunction(self.close, None),
+			"blue": self.SwaptoUSB,
 		}, -1)
 		self.onLayoutFinish.append(self.layoutFinished)
 
@@ -79,9 +78,19 @@ class H9SDswap(Screen):
 		else:
 			self.session.open(MessageBox, _("H9 SDcard switch ERROR! - H9 root files not transferred to SD card"), MessageBox.TYPE_INFO, timeout=20)
 
+	def SwaptoUSB(self):
+		self.switchtype = "usb"
+		f = open('/proc/cmdline', 'r').read()
+		print "[H9SDswap] switchtype %s cmdline %s" %(self.switchtype, f) 
+		if "root=/dev/SDA1" in f:
+			self.session.open(MessageBox, _("H9 USB switch ERROR! - already on USB"), MessageBox.TYPE_INFO, timeout=20)
+		elif os.path.isfile("/media/mmc/usr/bin/enigma2"):
+			self.container = Console()
+			self.container.ePopen("dd if=/usr/share/bootargs-usb.bin of=/dev/mtdblock1", self.Unm)
+		else:
+			self.session.open(MessageBox, _("H9 USB switch ERROR! - H9 root files not transferred to USB"), MessageBox.TYPE_INFO, timeout=20)
+
+
 	def Unm(self, data=None, retval=None, extra_args=None):
 		self.container.killAll()
-		self.session.open(TryQuitMainloop, 2)	
-
-	def reboot(self):
 		self.session.open(TryQuitMainloop, 2)
