db8369ac1e1dcbc9e0c72fac053c50b06d68c4aa
Tony Whalley <awhalley@waitrose.com>
2019-04-17 12:52:51 +0200

    SystemInfo correct multiboot

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -126,15 +126,12 @@ SystemInfo["CanAACTranscode"] = fileHas("/proc/stb/audio/aac_transcode_choices",
 SystemInfo["CanWMAPRO"] = fileHas("/proc/stb/audio/wmapro_choices", "downmix")
 #	Multiboot/bootmode options
 SystemInfo["HasRootSubdir"] = fileHas("/proc/cmdline", "rootsubdir=")
-#SystemInfo["canMultiBoot"] = SystemInfo["HasRootSubdir"] and (1, 4, 'mmcblk0p') or fileHas("/proc/cmdline", "_4.boxmode=") and (1, 4, "mmcblk0p") or getBoxType() in ('gbue4k', 'gbquad4k') and (3, 3, 'mmcblk0p') #or getMachineBuild() in ('sf8008', 'gbmv200', 'beyonwizv2') and fileCheck("/dev/sda") and (0, 2, 'sda') or getMachineBuild() in ('osmio4k') and pathExists("/dev/mmcblk1p5") and (1, 4, 'mmcblk1p')
-SystemInfo["canMultiBoot"] = getMachineBuild() in ('hd51', 'h7') and (1, 4, 'mmcblk0p') or getBoxType() in ('gbue4k', 'gbquad4k') and (3, 3, 'mmcblk0p') or getMachineBuild() in ('sf8008', 'gbmv200', beyonwizv2') and fileCheck("/dev/sda") and (0, 2, 'sda') or getMachineBuild() in ('osmio4k') and pathExists("/dev/mmcblk1p5") and (1, 4, 'mmcblk1p')
-#
-SystemInfo["canMode12"] = fileHas("/proc/cmdline", "_4.boxmode=") and ('440M@328M brcm_cma=192M@768M', '520M@248M brcm_cma=200M@768M')
-SystemInfo["canFlashWithOfgwrite"] = not SystemInfo["HasRootSubdir"]
+SystemInfo["canMultiBoot"] = getMachineBuild() in ('hd51', 'h7') and (1, 4, 'mmcblk0p') or getBoxType() in ('gbue4k', 'gbquad4k') and (3, 3, 'mmcblk0p') or getMachineBuild() in ('sf8008', 'gbmv200', 'beyonwizv2') and fileCheck("/dev/sda") and (0, 2, 'sda') or getMachineBuild() in ('osmio4k') and pathExists("/dev/mmcblk1p5") and (1, 4, 'mmcblk1p')
 SystemInfo["HasMMC"] = fileHas("/proc/cmdline", "root=/dev/mmcblk") or "mmcblk" in getMachineMtdRoot()
 SystemInfo["HasH9SD"] = getMachineBuild() in ("h9", "i55plus") and pathExists("/dev/mmcblk0p1")
 SystemInfo["HasMBSB"] = SystemInfo["canMultiBoot"] and fileExists("/usr/bin/8gb.sh")
 SystemInfo["HasSDmmc"] = SystemInfo["canMultiBoot"] and "sd" in SystemInfo["canMultiBoot"][2] and "mmcblk" in getMachineMtdRoot() 
 SystemInfo["HasHiSi"] = pathExists('/proc/hisi')
 SystemInfo["haveboxmode"] = fileExists("/proc/stb/info/boxmode")
+SystemInfo["canMode12"] = getMachineBuild() in ('hd51', 'h7') and ('440M@328M brcm_cma=192M@768M', '520M@248M brcm_cma=200M@768M')
 SystemInfo["CanProc"] = SystemInfo["HasMMC"] and getBrandOEM() != "vuplus"
