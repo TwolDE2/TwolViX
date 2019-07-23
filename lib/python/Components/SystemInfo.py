8ac9df80abe0098dbf1010e33647c71974ecba0a
Tony Whalley <awhalley@waitrose.com>
2019-07-23 14:00:38 +0200

    SystemInfo - remove Hashisi as this is a one off test for SF8008 not a HiSi generic

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -141,6 +141,5 @@ SystemInfo["canBackupEMC"] = getMachineBuild() in ('hd51','h7') and ('disk.img',
 SystemInfo["HasH9SD"] = getMachineBuild() in ("h9", "i55plus") and pathExists("/dev/mmcblk0p1")
 SystemInfo["HasSDmmc"] = SystemInfo["canMultiBoot"] and "sd" in SystemInfo["canMultiBoot"][2] and "mmcblk" in getMachineMtdRoot() 
 SystemInfo["HasSDnomount"] = getMachineBuild() in ('h9', 'i55plus') and ('No', 'none') or getMachineBuild() in ('multibox', 'h9combo') and ('Yes', 'mmcblk0')
-SystemInfo["HasHiSi"] = pathExists('/proc/hisi')
 SystemInfo["haveboxmode"] = fileExists("/proc/stb/info/boxmode")
 SystemInfo["canMode12"] = getMachineBuild() in ('hd51', 'h7') and ('brcm_cma=440M@328M brcm_cma=192M@768M', 'brcm_cma=520M@248M brcm_cma=200M@768M')
