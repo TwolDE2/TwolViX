aff3ee964fb6e5d854a63b1cb404778dc981a1ac
Tony Whalley <awhalley@waitrose.com>
2020-04-17 15:07:06 +0200

    StartWizard - work around language issue

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,15 +1,13 @@
-from Wizard import wizardManager
-from Screens.LanguageSelection import LanguageSelection
+from boxbranding import getBoxType
+from Components.config import config, ConfigBoolean, configfile
+#from Components.Language import Language
+from Components.Pixmap import Pixmap
+from Screens.LanguageSelection import LanguageWizard 
+from Screens.Rc import Rc
 from Screens.WizardLanguage import WizardLanguage
 from Screens.WizardUserInterfacePositioner import UserInterfacePositionerWizard
 from Screens.VideoWizard import VideoWizard
-from Screens.Rc import Rc
-from boxbranding import getBoxType
-
-from Components.Pixmap import Pixmap
-from Components.config import config, ConfigBoolean, configfile
-
-# from LanguageSelection import LanguageWizard
+from Wizard import wizardManager
 
 config.misc.firstrun = ConfigBoolean(default = True)
 config.misc.languageselected = ConfigBoolean(default = True)
