ff2f63cd10e74a5186d339d125639e4535351625
Tony Whalley <tonywhalley@t-online.de>
2020-08-11 12:50:58 +0200

    Revert "ebase comment out assert"
    This reverts commit 704fe2fb27ae9b8b476aa783a0f96167a2f23ebd.

--- lib/base/ebase.cpp
+++ lib/base/ebase.cpp
@@ -153,9 +153,8 @@ void eMainloop::addSocketNotifier(eSocketNotifier *sn)
 		*/
 		m_inActivate->stop();
 	}
-/*	ASSERT(notifiers.find(fd) == notifiers.end());
+	ASSERT(notifiers.find(fd) == notifiers.end());
 	notifiers[fd]=sn;
-*/
 }
 
 void eMainloop::removeSocketNotifier(eSocketNotifier *sn)
