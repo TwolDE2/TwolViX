c41fa6e0b5481f42b01070eedf331346cbbcc427
IanSav <IanSav@users.noreply.github.com>
2020-09-12 18:36:42 +1000

    [Setup.py] Correct skin image widget name

--- lib/python/Screens/Setup.py
+++ lib/python/Screens/Setup.py
@@ -56,7 +56,7 @@ class Setup(ConfigListScreen, Screen, HelpableScreen):
 			setupImage = resolveFilename(SCOPE_CURRENT_SKIN, setupImage)
 			self.setupImage = LoadPixmap(setupImage)
 			if self.setupImage:
-				self["menuimage"] = Pixmap()
+				self["setupimage"] = Pixmap()
 			else:
 				print("[Setup] Error: Unable to load menu image '%s'!" % setupImage)
 		else:
@@ -221,7 +221,7 @@ class Setup(ConfigListScreen, Screen, HelpableScreen):
 
 	def layoutFinished(self):
 		if self.setupImage:
-			self["menuimage"].instance.setPixmap(self.setupImage)
+			self["setupimage"].instance.setPixmap(self.setupImage)
 		if not self["config"]:
 			print("[Setup] No setup items available!")
 
