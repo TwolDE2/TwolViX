openatv/enigma2

[InfoBarGenerics, ChannelSelection] Fix timeouts
In InfoBarGenerics & ChannelSelection fix timeouts that use
config.usage.infobar_timeout and config.usage.second_infobar_timeout
so that they use int(config. ... .value) instead of config. ... .index.

lib/python/Screens/InfoBarGenerics.py
@@ -537,21 +537,21 @@ class InfoBarShowHide(InfoBarScreenSaver):
 		if self.__state == self.STATE_SHOWN and not self.__locked:
 			self.hideTimer.stop()
 			if self.secondInfoBarScreen and self.secondInfoBarScreen.shown:
-				idx = config.usage.second_infobar_timeout.index
+				val = int(config.usage.second_infobar_timeout.value)
 			else:
-				idx = config.usage.infobar_timeout.index
-			if idx:
-				self.hideTimer.start(idx * 1000, True)
+				val = int(config.usage.infobar_timeout.value)
+			if val:
+				self.hideTimer.start(val * 1000, True)
 		elif (self.secondInfoBarScreen and self.secondInfoBarScreen.shown) or ((not config.usage.show_second_infobar.value or isMoviePlayerInfoBar(self))):
 			self.hideTimer.stop()
-			idx = config.usage.second_infobar_timeout.index
-			if idx:
-				self.hideTimer.start(idx * 1000, True)
+			val = int(config.usage.second_infobar_timeout.value)
+			if val:
+				self.hideTimer.start(val * 1000, True)
 		elif hasattr(self, "pvrStateDialog"):
 			self.hideTimer.stop()
-			idx = config.usage.infobar_timeout.index
-			if idx:
-				self.hideTimer.start(idx * 1000, True)
+			val = int(config.usage.infobar_timeout.value)
+			if val:
+				self.hideTimer.start(val * 1000, True)
 
 	def doShow(self):
 		self.show()

TwolDE2/TwolViX

lib/python/Screens/InfoBarGenerics.py
@@ -690,19 +690,19 @@ class InfoBarShowHide(InfoBarScreenSaver):
 	def startHideTimer(self):
 		if self.__state == self.STATE_SHOWN and not self.__locked:
 			self.hideTimer.stop()
-			idx = config.usage.infobar_timeout.index
-			if idx:
-				self.hideTimer.start(idx*1000, True)
+			val = int(config.usage.infobar_timeout.value)
+			if val:
+				self.hideTimer.start(val * 1000, True)
 		elif (self.secondInfoBarScreen and self.secondInfoBarScreen.shown) or ((not config.usage.show_second_infobar.value or isMoviePlayerInfoBar(self)) and self.EventViewIsShown):
 			self.hideTimer.stop()
-			idx = config.usage.show_second_infobar.index - 1
-			if idx:
-				self.hideTimer.start(idx*1000, True)
+			val = int(config.usage.show_second_infobar.value)
+			if val:
+				self.hideTimer.start(val * 1000, True)
 		elif hasattr(self, "pvrStateDialog"):
 			self.hideTimer.stop()
-			idx = config.usage.infobar_timeout.index
-			if idx:
-				self.hideTimer.start(idx*1000, True)
+			val = int(config.usage.infobar_timeout.value)
+			if val:
+				self.hideTimer.start(val * 1000, True)
 
 	def doShow(self):
 		self.show()