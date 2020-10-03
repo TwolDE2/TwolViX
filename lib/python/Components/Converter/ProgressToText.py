cf8257e36ab9784dbdb9ddb591c41d630c58700f
Tony Whalley <tonywhalley@t-online.de>
2020-10-03 11:06:57 +0200

    ProgressToText - this is not division - this is text!

--- lib/python/Components/Converter/ProgressToText.py
+++ lib/python/Components/Converter/ProgressToText.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-from __future__ import division
 
 from Components.Converter.Converter import Converter
 from Components.Element import cached
@@ -16,10 +15,10 @@ class ProgressToText(Converter, object):
 
 		if self.in_percent:
 			if r:
-				return "%d %%" % (v * 100 // r)
+				return "%d %%" % (v * 100 / r)
 			else:
 				return None
 		else:
-			return "%d // %d" % (v, r)
+			return "%d / %d" % (v, r)
 
 	text = property(getText)
