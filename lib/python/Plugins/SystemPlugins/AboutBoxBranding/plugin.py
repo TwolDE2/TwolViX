88062dacda3df01153a1df96cf7926727609017e
Tony Whalley <tonywhalley@t-online.de>
2020-12-08 13:49:51 +0100

    AboutBoxBranding - change debug output to print in columns

--- lib/python/Plugins/SystemPlugins/AboutBoxBranding/plugin.py
+++ lib/python/Plugins/SystemPlugins/AboutBoxBranding/plugin.py
@@ -1,5 +1,6 @@
 # for localized messages
-import six
+from __future__ import print_function
+
 from . import _
 
 from Components.ActionMap import ActionMap
@@ -16,7 +17,7 @@ class AboutBoxBranding(Screen):
 		self.skinName = ["AboutBoxBranding", "AboutOE", "About"]
 
 		self["key_red"] = StaticText(_("Close"))
-		self["AboutScrollLabel"] = ScrollLabel(getBoxbranding())
+		self["AboutScrollLabel"] = ScrollLabel(getBoxbranding()[0])
 
 		self["actions"] = ActionMap(["WizardActions", "ColorActions"],
 		{
@@ -37,16 +38,17 @@ class AboutBoxBranding(Screen):
 def getBoxbranding():
 	import boxbranding
 	bblist = []
+	prlist = []
 	for m in sorted(boxbranding.__dict__.keys()):
 		if callable(getattr(boxbranding, m)):
 			v = getattr(boxbranding, m)()
-			v = six.ensure_str(v)
 			for x in ("http://", "https://"): # Trim URLs to domain only
 				if v.startswith(x):
 					v = v.split(x)[1].split('/')[0] + " [...]"
 					break
-			bblist.append("%s:\t %s\n" % (m, v))					
-	return ''.join(bblist)
+			bblist.append("%s:\t %s\n" % (m, v))
+			prlist.append(tuple([m, v]))
+	return (''.join(bblist), prlist)
 
 def main(session, **kwargs):
 	session.open(AboutBoxBranding)
@@ -57,8 +59,13 @@ def start(menuid):
 	return []
 
 def Plugins(**kwargs):
+	prlist = []
 	try:
-		print("[AboutBoxBranding]...\n%s" % getBoxbranding())
+		prlist = getBoxbranding()[1]
+		res = len(prlist) 
+		print("[AboutBoxBranding]... Number of entries = %s" % (res))
+		for x in range(0, res):
+			print("{: <24} {: <20}".format(prlist[x][0], prlist[x][1]))
 	except Exception as err:
 		print("[AboutBoxBranding] Error: %s: '%s'!" % (type(err).__name__, err))
 		return []
