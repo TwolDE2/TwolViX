ef349de2d880e1843a5032e93486bcd04c1a3e3f
tony whalley <awhalley@waitrose.com>
2021-04-01 08:40:34 +0200

    Revert "Rename mytest to Startup"
    This reverts commit 1d053a0284df1a4bb6b704e79b829d4e422cc024.

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -353,7 +353,8 @@ int main(int argc, char **argv)
 	/* start at full size */
 	eVideoWidget::setFullsize(true);
 
-	python.execFile(eEnv::resolve("${libdir}/enigma2/python/Bootstrap.py").c_str());
+	// python.execute("mytest", "__main__");
+	python.execFile(eEnv::resolve("${libdir}/enigma2/python/mytest.py").c_str());
 
 	/* restore both decoders to full size */
 	eVideoWidget::setFullsize(true);
