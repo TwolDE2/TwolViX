e0684302aceb9482e1920a46bb7dc6dce6c65d3c
Huevos <rowland@moralet.com>
2021-04-21 16:27:00 +0200

    [LoadPixmap] Add new skin parameter "AutoscaleSVG" (#2937)
    <parameter name="AutoscaleSVG" value="1"/>
    
    SVG images are scaled based on the width/height, but if width/height are "0" the image will be automatically scaled based on the "AutoscaleSVG" skin parameter.
    
    There are 3 options:
    -1 : only auto scale SVG graphics if "/skin_default/" is in the path (this is the default),
     0 : never auto scale SVG graphics,
     1 : always auto scale SVG graphics

--- lib/python/Tools/LoadPixmap.py
+++ lib/python/Tools/LoadPixmap.py
@@ -1,4 +1,4 @@
-from enigma import loadPNG, loadJPG, loadSVG, getDesktop
+from enigma import loadPNG, loadJPG, loadSVG
 
 
 # Return a scaling factor (float) that can be used to rescale images
@@ -19,7 +19,10 @@ def LoadPixmap(path, desktop=None, cached=None, width=0, height=0):
 		# don't cache unless caller explicity requests caching
 		ptr = loadJPG(path, 1 if cached is True else 0)
 	elif path[-4:] == ".svg":
-		ptr = loadSVG(path, 0 if cached is False else 1, width, height, SKIN_FACTOR)
+		from skin import parameters, getSkinFactor # imported here to avoid circular import
+		autoscale = int(parameters.get("AutoscaleSVG", -1)) # skin_default only == -1, disabled == 0 or enabled == 1
+		scale = height == 0 and (autoscale == -1 and "/skin_default/" in path or autoscale == 1) and getSkinFactor() or 0
+		ptr = loadSVG(path, 0 if cached is False else 1, width, height, scale)
 	elif path[-1:] == ".":
 		# caching mechanism isn't suitable for multi file images, so it's explicitly disabled
 		alpha = loadPNG(path + "a.png", 0, 0)
