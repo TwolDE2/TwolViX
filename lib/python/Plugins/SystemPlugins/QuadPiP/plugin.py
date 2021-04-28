6dd6e7945f495efbfeb42f180ee5985502854592
tony whalley <awhalley@waitrose.com>
2021-04-28 09:40:01 +0200

    QuadPiP - add plugin to SystemPlugins

---
+++ lib/python/Plugins/SystemPlugins/QuadPiP/plugin.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+
+from Plugins.Plugin import PluginDescriptor
+from Components.PluginComponent import plugins
+from enigma import eDBoxLCD
+
+from .qpip import QuadPipScreen, setDecoderMode
+
+
+def main(session, **kwargs):
+	session.open(QuadPipScreen)
+
+
+def autoStart(reason, **kwargs):
+	if reason == 0:
+		setDecoderMode("normal")
+	elif reason == 1:
+		setDecoderMode("normal")
+
+
+def Plugins(**kwargs):
+	list = []
+	list.append(
+
+		PluginDescriptor(name=_("Enable Quad PiP"),
+		description=_("Quad Picture in Picture"),
+		where=[PluginDescriptor.WHERE_EXTENSIONSMENU],
+		fnc=main))
+
+	list.append(
+		PluginDescriptor(
+		where=[PluginDescriptor.WHERE_AUTOSTART],
+		fnc=autoStart))
+
+	return list
