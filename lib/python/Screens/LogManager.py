8805f89d43ee6b0f43b18c802b66066a2b38ef4e
Gordon Lack <gordon.lack@dsl.pipex.com>
2021-08-26 02:22:45 +0100

    Enable log rotation to prevent log corruption.

--- lib/python/Screens/LogManager.py
+++ lib/python/Screens/LogManager.py
@@ -75,11 +75,9 @@ class LogManagerPoller:
 			self.TrashTimer.callback.remove(self.TrashTimerJob)
 		self.TrashTimer.stop()
 
-
 	def TrashTimerJob(self):
 		print("[LogManager] Trash Poll Started")
 		Components.Task.job_manager.AddJob(self.createTrashJob())
-
 
 	def createTrashJob(self):
 		job = Components.Task.Job(_("LogManager"))
