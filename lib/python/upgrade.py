6ebaf43253a85876f9e10762331f6a3385c807e7
Twol <tonywhalley@t-online.de>
2021-12-24 12:13:24 +0100

    upgrade.py - fix typo

--- lib/python/upgrade.py
+++ lib/python/upgrade.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from os import listdir, path as os path, system
+from os import listdir, path as ospath, system
 
 opkgDestinations = ['/']
 opkgStatusPath = ''
