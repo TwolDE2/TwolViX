42a5d779d3c616a0b7daa943bd3378d74a312647
Twol <tonywhalley@t-online.de>
2022-01-20 10:40:02 +0100

    message.h - change fatal msg

--- lib/base/message.h
+++ lib/base/message.h
@@ -152,7 +152,7 @@ public:
 	{
 		static const uint64_t data = 1;
 		if (::write(m_fd, &data, sizeof(data)) < 0)
-			eFatal("[eFixedMessagePump<%s>] write error %m", name);
+			eeWarning("[eFixedMessagePump<%s>] write error %m", name);
 	}
 public:
 	sigc::signal1<void,const T&> recv_msg;
