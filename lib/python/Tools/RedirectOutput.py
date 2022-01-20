e0a8162989426c15bc440ce232bc6798b36d4a29
Twol <tonywhalley@t-online.de>
2022-01-20 15:03:26 +0100

    RedirectOutput - redo binary decode

--- lib/python/Tools/RedirectOutput.py
+++ lib/python/Tools/RedirectOutput.py
@@ -1,4 +1,3 @@
-import six
 import sys
 from enigma import ePythonOutput
 
@@ -9,7 +8,12 @@ class EnigmaLog:
 		self.line = ""
 
 	def write(self, data):
-		six.ensure_str(encoding="UTF-8", errors="ignore")
+		if sys.version_info[0] >= 3:
+			if isinstance(data, bytes):
+				data = data.decode(encoding="UTF-8", errors="ignore")
+		else:
+			if isinstance(data, unicode):
+				data = data.encode(encoding="UTF-8", errors="ignore")
 		self.line += data
 		if "\n" in data:
 			ePythonOutput(self.line, self.level)
