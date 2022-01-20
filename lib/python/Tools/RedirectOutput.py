d686a8bbf3a71b3633bde2ed6ed9d06494a8d021
Twol <tonywhalley@t-online.de>
2022-01-20 11:23:51 +0100

    RedirectOutput - fix encoding for bytes

--- lib/python/Tools/RedirectOutput.py
+++ lib/python/Tools/RedirectOutput.py
@@ -9,12 +9,7 @@ class EnigmaLog:
 		self.line = ""
 
 	def write(self, data):
-		if six.PY3:
-			if isinstance(data, bytes):
-				data = data.encode(encoding="UTF-8", errors="ignore")
-		else:
-			if isinstance(data, unicode):
-				data = data.encode(encoding="UTF-8", errors="ignore")
+		six.ensure_str(encoding="UTF-8", errors="ignore")
 		self.line += data
 		if "\n" in data:
 			ePythonOutput(self.line, self.level)
