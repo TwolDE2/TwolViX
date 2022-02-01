f04c00fd9fd8189e927b014b604081d6209cc286
Athanasios Oikonomou <athoik@gmail.com>
2022-02-01 22:45:58 +0200

    message.h - correct sig3 change

--- lib/base/message.h
+++ lib/base/message.h
@@ -91,7 +91,7 @@ class eFixedMessagePump: public sigc::trackable, FD
 		}
 	}
 public:
-	sigc::signal1<void,const T&> recv_msg;
+	sigc::signal<void(const T&)> recv_msg;
 	void send(const T &msg)
 	{
 		{
