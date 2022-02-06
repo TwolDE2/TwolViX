e7b9974b11d093c2bb80163c899014672201fbc3
Twol <tonywhalley@t-online.de>
2022-02-06 11:29:49 +0100

    Remove unused import "six"

--- lib/python/Components/MovieList.py
+++ lib/python/Components/MovieList.py
@@ -1,4 +1,3 @@
-import six
 from six.moves import reload_module
 
 from os import path, stat
--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -1,5 +1,3 @@
-import six
-
 from os import access, path, F_OK 
 from datetime import datetime
 from time import localtime, mktime
