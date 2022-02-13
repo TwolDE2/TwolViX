2ba61f69b1d474f17b8b650207d9dfd63056ddcf
Huevos <rowland@moralet.com>
2022-02-14 00:23:03 +0100

    Network password: Remove not required callback

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -3468,7 +3468,7 @@ class NetworkPassword(ConfigListScreen, Screen):
 		self.skinName = "Setup"
 		self.onChangedEntry = []
 		self.list = []
-		ConfigListScreen.__init__(self, self.list, session=self.session, on_change=self.selectionChanged)
+		ConfigListScreen.__init__(self, self.list, session=self.session)
 
 		self["key_red"] = StaticText(_("Exit"))
 		self["key_green"] = StaticText(_("Save"))
