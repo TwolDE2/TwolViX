5d198706d47c0af34152bd734b8f156c4e22876e
Twol <tonywhalley@t-online.de>
2022-03-16 16:46:59 +0100

    ISO639 remove version - fix indent

--- lib/python/Tools/ISO639.py
+++ lib/python/Tools/ISO639.py
@@ -1,5 +1,4 @@
 import pickle
 import enigma
 with open(enigma.eEnv.resolve("${datadir}/enigma2/iso-639-3.pck"), 'rb') as f:
-	if version_info >= (3, 0):
-LanguageCodes = pickle.load(f, encoding='utf-8')
+	LanguageCodes = pickle.load(f, encoding='utf-8')
