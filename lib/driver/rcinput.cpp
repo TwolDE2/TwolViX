d85d0de8ee5bc53e47fe5693605369f373965548
Twol <tonywhalley@t-online.de>
2022-05-01 10:48:29 +0200

    Revert "gbtrio4K - try fix key_subt"
    This reverts commit 0eacf7dae07bea3d464676b38ad0d82a578438fc.

--- lib/driver/rcinput.cpp
+++ lib/driver/rcinput.cpp
@@ -17,9 +17,9 @@ void eRCDeviceInputDev::handleCode(long rccode)
 {
 	struct input_event *ev = (struct input_event *)rccode;
 	eDebug("[eInputDeviceInit0] %x %x (%u) %x", ev->value, ev->code, ev->code, ev->type);
-	if (ev->type != EV_KEY && ev->value != "0x50afa15e")
+	if (ev->type != EV_KEY)
 		return;
-	eDebug("[eInputDeviceInit0a] %x %x (%u) %x", ev->value, ev->code, ev->code, ev->type);	
+		
 	int km = iskeyboard ? input->getKeyboardMode() : eRCInput::kmNone;
 
 	switch (ev->code)
@@ -107,15 +107,6 @@ void eRCDeviceInputDev::handleCode(long rccode)
 			ev->code = KEY_MEDIA;
 		}
 #endif
-
-#if KEY_SUBT_TO_KEY_SUBTITLE
-		if (ev->value == "0x50afa15e")
-		{
-			/* Gigablue gbuhd4K/gbue4k Remote rc has a KEY_SUBT, which sends No events. Correct this, so we do not have to place hacks in the keymaps. */
-			ev->code = KEY_SUBTITLE;
-		}
-#endif
-
 
 #if KEY_CONTEXT_MENU_TO_KEY_AUX
 		if (ev->code == KEY_CONTEXT_MENU)
