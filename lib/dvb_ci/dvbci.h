dea2c08d6c7ec50b280c01c87ef380472d7bdc7e
Twol <tonywhalley@t-online.de>
2022-05-06 15:12:50 +0200

    ForceFbcLNBpower - dvb_ci.h fix missing statement

--- lib/dvb_ci/dvbci.h
+++ lib/dvb_ci/dvbci.h
@@ -89,7 +89,7 @@ class eDVBCISlot: public iObject, public sigc::trackable
 	void determineCIVersion();
 	static std::string getTunerLetter(int tuner_no) { return std::string(1, char(65 + tuner_no)); }
 public:
-	enum {stateRemoved, stateInserted, stateInvalid, stateResetted};
+	enum {stateRemoved, stateInserted, stateInvalid, stateResetted, stateDisabled};
 	enum {versionUnknown = -1, versionCI = 0, versionCIPlus1 = 1, versionCIPlus2 = 2};
 	eDVBCISlot(eMainloop *context, int nr);
 	~eDVBCISlot();
