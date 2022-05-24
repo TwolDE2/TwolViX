eae64223a87f216731a739450cc1723830fc54e8
Twol <tonywhalley@t-online.de>
2022-05-24 19:29:19 +0200

    Wizard remove whitespace

--- lib/python/Screens/Wizard.py
+++ lib/python/Screens/Wizard.py
@@ -233,7 +233,6 @@ class Wizard(Screen):
 			"9": self.keyNumberGlobal,
 			"0": self.keyNumberGlobal
 		}, -1)
-
 		self["VirtualKB"] = NumberActionMap(["VirtualKeyboardActions"],
 		{
 			"showVirtualKeyboard": self.KeyText,
