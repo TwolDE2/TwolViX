22426c674c7b364b93043dacede3f7a57740e84f
Twol <tonywhalley@t-online.de>
2022-06-10 15:50:58 +0200

    Revert "Downloader - add machine info"
    This reverts commit f4ca0b3c28a284d4b3261020a2cd5f8575f29034.

--- lib/python/Tools/Downloader.py
+++ lib/python/Tools/Downloader.py
@@ -4,13 +4,11 @@ from twisted.internet import reactor
 from urllib.request import urlopen, Request
 from enigma import eTimer
 
-from boxbranding import getMachineBrand, getMachineName
-
 class DownloadWithProgress:
 	def __init__(self, url, outputFile):
 		self.url = url
 		self.outputFile = outputFile
-		self.userAgent=("%s %s Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenViX;;;)" % (getMachineBrand(), getMachineName()))
+		self.userAgent = "Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenATV;;;)"
 		# self.agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5"
 		self.totalSize = 0
 		self.progress = 0
@@ -64,11 +62,13 @@ class DownloadWithProgress:
 	def stop(self):
 		self.stopFlag = True
 
+
 	def reportProgress(self):
 		self.progressCallback(self.progress, self.totalSize)
 
 	def addProgress(self, progressCallback):
 		self.progressCallback = progressCallback
+
 
 	def addEnd(self, endCallback):
 		self.endCallback = endCallback
@@ -91,5 +91,5 @@ class DownloadWithProgress:
 		return self
 
 
-class downloadWithProgress(DownloadWithProgress):  # support old code
+class downloadWithProgress(DownloadWithProgress):  # Class names should start with a Capital letter, this catches old code until that code can be updated.
 	pass
