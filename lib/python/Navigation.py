2cb7b5f76e645e3fef783031289e091b963841c2
Twol <tonywhalley@t-online.de>
2022-09-05 11:01:03 +0200

    Navigation remove FCC code - self.skipServRefReset

--- lib/python/Navigation.py
+++ lib/python/Navigation.py
@@ -38,7 +38,6 @@ class Navigation:
 		self.currentlyPlayingServiceReference = None
 		self.currentlyPlayingServiceOrGroup = None
 		self.currentlyPlayingService = None
-		self.skipServRefReset = False
 		self.RecordTimer = RecordTimer.RecordTimer()
 		self.PowerTimer = PowerTimer.PowerTimer()
 		self.__wasTimerWakeup = False
@@ -91,9 +90,8 @@ class Navigation:
 		for x in self.event:
 			x(i)
 		if i == iPlayableService.evEnd:
-			if not self.skipServRefReset:
-				self.currentlyPlayingServiceReference = None
-				self.currentlyPlayingServiceOrGroup = None
+			self.currentlyPlayingServiceReference = None
+			self.currentlyPlayingServiceOrGroup = None
 			self.currentlyPlayingService = None
 
 	def dispatchRecordEvent(self, rec_service, event):
@@ -198,7 +196,6 @@ class Navigation:
 								if config.usage.frontend_priority_dvbs.value != config.usage.frontend_priority.value:
 									setPreferredTuner(int(config.usage.frontend_priority_dvbs.value))
 									setPriorityFrontend = True
-				self.skipServRefReset = True
 				if self.pnav.playService(playref):
 				#	print("[Navigation] Failed to start", playref)
 					self.currentlyPlayingServiceReference = None
@@ -208,7 +205,6 @@ class Navigation:
 						self.retryServicePlayTimer = eTimer()
 						self.retryServicePlayTimer.callback.append(boundFunction(self.playService, ref, checkParentalControl, forceRestart, adjust))
 						self.retryServicePlayTimer.start(500, True)
-				self.skipServRefReset = False
 				if setPriorityFrontend:
 					setPreferredTuner(int(config.usage.frontend_priority.value))
 				return 0
