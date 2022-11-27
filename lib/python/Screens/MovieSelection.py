a9e0d072f5d6dc3474f652108e77c08e2488736f
Rowland Creed <rowland@moralet.com>
2022-11-27 18:29:59 +0100

    [MovieSelection] createMoveList(), check for correct file extension
    Conflicts:
    	lib/python/Screens/MovieSelection.py

--- lib/python/Screens/MovieSelection.py
+++ lib/python/Screens/MovieSelection.py
@@ -161,10 +161,10 @@ canRename = canMove
 
 
 def createMoveList(serviceref, dest):
-	#normpath is to remove the trailing "/" from directories
-	src = isinstance(serviceref, str) and serviceref + ".ts" or ospath.normpath(serviceref.getPath())
-	srcPath, srcName = ospath.split(src)
-	if ospath.normpath(srcPath) == dest:
+	#normpath is to remove the trailing '/' from directories
+	src = isinstance(serviceref, str) and "%s.%s" % (serviceref, "ts" if os.path.exists("%s.ts" % serviceref) else "stream") or os.path.normpath(serviceref.getPath())
+	srcPath, srcName = os.path.split(src)
+	if os.path.normpath(srcPath) == dest:
 		# move file to itself is allowed, so we have to check it
 		raise Exception("Refusing to move to the same directory")
 	# Make a list of items to move
