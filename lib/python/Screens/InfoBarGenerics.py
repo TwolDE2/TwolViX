9855f075a8409291f5dcd2b991204f5bc5724757
Rowland Creed <rowland@moralet.com>
2022-12-26 22:57:51 +0100

    [InfoBarGenerics] instant record (when not event ): set message locale
    Thx  @Dima73

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -3226,7 +3226,7 @@ class InfoBarInstantRecord:
 	def startInstantRecording(self, limitEvent=False):
 		begin = int(time())
 		end = begin + 3600 # dummy
-		name = "instant record"
+		name = _("instant record")
 		info = {}
 
 		self.getProgramInfoAndEvent(info, name)
