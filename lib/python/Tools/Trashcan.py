bf8220f1a6bf7f95528c864a49c9e04d8f4dabe8
Rowland Creed <rowland@moralet.com>
2023-01-04 22:04:51 +0100

    [Trashcan] if file does not stat continue
    Conflicts:
    	lib/python/Tools/Trashcan.py

--- lib/python/Tools/Trashcan.py
+++ lib/python/Tools/Trashcan.py
@@ -183,7 +183,6 @@ class CleanTrashTask(Components.Task.PythonTask):
 						except FileNotFoundError:						
 							print("[Trashcan][CleanTrashTask[work]  FileNotFoundError ", fn)
 							continue
-#						print("[Trashcan][CleanTrashTask[work]  fn = ", fn)
 						if st.st_ctime < self.ctimeLimit or config.usage.movielist_trashcan_days.value == 0:
 							enigma.eBackgroundFileEraser.getInstance().erase(fn)
 							bytesToRemove -= st.st_size
