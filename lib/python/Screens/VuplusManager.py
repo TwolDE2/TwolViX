0acb87d01de4262b1f3d987664b975f9c28f1cd5
Twol <tonywhalley@t-online.de>
2023-02-02 11:39:57 +0100

    Complete text changes

--- lib/python/Screens/VuplusManager.py
+++ lib/python/Screens/VuplusManager.py
@@ -18,22 +18,43 @@ class VuplusManager(Screen):
 
 	skin = """
 	<screen name="VuplusManager" position="center,center" size="750,700" flags="wfNoBorder" backgroundColor="transparent">
+		<eLabel name="b" position="0,0" size="750,700" backgroundColor="#00ffffff" zPosition="-2" />
+		<eLabel name="a" position="1,1" size="748,698" backgroundColor="#00000000" zPosition="-1" />
 		<widget source="Title" render="Label" position="60,10" foregroundColor="#00ffffff" size="480,50" halign="left" font="Regular; 28" backgroundColor="#00000000" />
+		<eLabel name="line" position="1,60" size="748,1" backgroundColor="#00ffffff" zPosition="1" />
+		<eLabel name="line2" position="1,250" size="748,4" backgroundColor="#00ffffff" zPosition="1" />
+		<widget source="labe14" render="Label" position="2,80" size="730,30" halign="center" font="Regular; 22" backgroundColor="#00000000" foregroundColor="#00ffffff" />
+		<widget source="key_red" render="Label" position="30,200" size="150,30" noWrap="1" zPosition="1" valign="center" font="Regular; 20" halign="left" backgroundColor="#00000000" foregroundColor="#00ffffff" />
+		<widget source="key_green" render="Label" position="200,200" size="150,30" noWrap="1" zPosition="1" valign="center" font="Regular; 20" halign="left" backgroundColor="#00000000" foregroundColor="#00ffffff" />
+		<widget source="key_yellow" render="Label" position="370,200" size="150,30" noWrap="1" zPosition="1" valign="center" font="Regular; 20" halign="left" backgroundColor="#00000000" foregroundColor="#00ffffff" />
+		<ePixmap pixmap="skin_default/buttons/green.png" position="200,200" size="40,40" alphatest="blend" />
 	</screen>
 	"""
 
 	def __init__(self, session, *args, **kwargs):
 		Screen.__init__(self, session)
-		self["actions"] = ActionMap(["OkCancelActions"],
+		self.skinName = "VuplusManager"
+		self.setTitle(_("Vu+ MultiBoot Manager"))
+		self["labe14"] = StaticText(_("Press Green key to enable MultiBoot."))
+		self["key_red"] = StaticText(_(" "))
+		self["key_green"] = StaticText(_("Init Vu+ MultiBoot"))
+		self["key_yellow"] = StaticText(" ")
+		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"],
 		{
+			"red": self.close,
+			"green": self.RootInit,
+			"yellow": self.close,
 			"ok": self.close,
 			"cancel": self.close,
 		}, -1)
-		self.onLayoutFinish.append(self.RootInit)
+		self.onLayoutFinish.append(self.layoutFinished)
+
+	def layoutFinished(self):
+		self.setTitle(_("VU+ MultiBoot Manager"))
 
 	def RootInit(self):
 		if SystemInfo["CanKexecVu"]:
-			self.TITLE = _("MultiBoot Init Vu+  - please reboot after use.")
+			self.TITLE = _("Vu+ MultiBoot Inititialisation - will reboot after 10 seconds.")
 			with open("/STARTUP", 'w') as f:
 				f.write(STARTUP)
 			with open("/STARTUP_RECOVERY", 'w') as f:
@@ -59,4 +80,3 @@ class VuplusManager(Screen):
 
 	def Reboot(self):
 		self.session.open(TryQuitMainloop, 2)
-
