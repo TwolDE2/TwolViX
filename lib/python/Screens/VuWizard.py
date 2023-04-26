f728c0b8e0d451c1011697808cbd45dc1841aedb
Twol <tonywhalley@t-online.de>
2023-04-26 11:52:27 +0200

    VuWizard - revert

--- lib/python/Screens/VuWizard.py
+++ lib/python/Screens/VuWizard.py
@@ -1,7 +1,6 @@
 import glob
 from time import sleep
 from boxbranding import getBoxType, getMachineMtdKernel, getMachineMtdRoot
-from Components.config import config, configfile
 from Components.Console import Console
 from Components.Label import Label
 from Components.Sources.StaticText import StaticText
@@ -71,9 +70,4 @@ class VuWizard(Screen):
 			self.reBoot()					
 
 	def reBoot(self, *args, **kwargs):
-		config.misc.restorewizardrun = True
-		config.misc.restorewizardrun.save()
-		config.misc.firstrun.value = 0
-		config.misc.firstrun.save()		
-		configfile.save()					
 		self.Console.ePopen("killall -9 enigma2 && init 6")
