645917dec1429b7c9f134482a8e8dda89b84699e
Twol <tonywhalley@t-online.de>
2023-06-28 15:05:26 +0200

    Bsod - assume glibc

--- main/bsod.cpp
+++ main/bsod.cpp
@@ -297,7 +297,6 @@ void oops(const mcontext_t &context)
  */
 void print_backtrace()
 {
-#ifdef __GLIBC__
 	void *array[15];
 	size_t size;
 	size_t cnt;
@@ -314,7 +313,6 @@ void print_backtrace()
 			eLog(lvlFatal, "%s(%s) [0x%X]", info.dli_fname, info.dli_sname != NULL ? info.dli_sname : "n/a", (unsigned long int) array[cnt]);
 		}
 	}
-#endif
 }
 
 void handleFatalSignal(int signum, siginfo_t *si, void *ctx)
