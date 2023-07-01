3b11c1e1158a9731f34375bae7bf0fab4a6d6b7f
Twol <tonywhalley@t-online.de>
2023-07-01 14:19:19 +0200

    AVSwitch - extend debug

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -238,7 +238,8 @@ class AVSwitch:
 	def setConfiguredMode(self):
 		port = config.av.videoport.value
 		if port not in config.av.videomode:
-			print("[AVSwitch] current port not available, not setting videomode")
+			print("[AVSwitch] current port (%s) not available, not setting videomode" % port)
+			print("[AVSwitch] current port not available, config.av.videomode", config.av.videomode)			
 			return
 		mode = config.av.videomode[port].value
 		if mode not in config.av.videorate:
@@ -276,7 +277,8 @@ class AVSwitch:
 		ret = (16, 9)
 		port = config.av.videoport.value
 		if port not in config.av.videomode:
-			print("[AVSwitch] current port not available in getOutputAspect!!! force 16:9")
+			print("[AVSwitch] current port (%s) not available in getOutputAspect!!! force 16:9" % port)
+			print("[AVSwitch] current port not available config.av.videomode", config.av.videomode)			
 		else:
 			mode = config.av.videomode[port].value
 			force_widescreen = self.isWidescreenMode(port, mode)
