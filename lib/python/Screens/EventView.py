3d17c888fce20389ea181915363a6f71f840a1b8
Rowland Creed <rowland@moralet.com>
2023-07-18 03:19:17 +0200

    [EventView] Allow display of plugin description if available in EventViewContextMenu
    Conflicts:
    	lib/python/Screens/EventView.py

--- lib/python/Screens/EventView.py
+++ lib/python/Screens/EventView.py
@@ -29,6 +29,8 @@ class EventViewContextMenu(Screen):
 		Screen.__init__(self, session)
 		self.setTitle(_("Event view menu"))
 
+		self["description"] = StaticText()
+
 		self["actions"] = ActionMap(["OkCancelActions"],
 			{
 				"ok": self.okbuttonClick,
@@ -36,12 +38,19 @@ class EventViewContextMenu(Screen):
 			})
 
 		self["menu"] = MenuList(menu)
+		if self.updateDescription not in self["menu"].onSelectionChanged:
+			self["menu"].onSelectionChanged.append(self.updateDescription)
+		self.updateDescription()
 
 	def okbuttonClick(self):
 		self["menu"].getCurrent() and self["menu"].getCurrent()[1]()
 
 	def cancelClick(self):
 		self.close(False)
+
+	def updateDescription(self):
+		current = self["menu"].getCurrent()
+		self["description"].text = current and len(current) > 2 and hasattr(current[2], "description") and current[2].description or ""
 
 
 class EventViewBase:
@@ -257,7 +266,7 @@ class EventViewBase:
 			for p in plugins.getPlugins(PluginDescriptor.WHERE_EVENTINFO):
 				#only list service or event specific eventinfo plugins here, no servelist plugins
 				if "servicelist" not in p.fnc.__code__.co_varnames:
-					menu.append((p.name, boundFunction(self.runPlugin, p)))
+					menu.append((p.name, boundFunction(self.runPlugin, p), p))
 			if menu:
 				self.session.open(EventViewContextMenu, menu)
 
