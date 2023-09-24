ba20b8efee98377143bdea582435c1bbc0e9ad98
Twol <tonywhalley@t-online.de>
2023-09-24 14:48:52 +0200

    PowerTimerEdit is not ==

--- lib/python/Screens/PowerTimerEdit.py
+++ lib/python/Screens/PowerTimerEdit.py
@@ -7,7 +7,6 @@ from Components.config import config
 from Components.Label import Label
 from Components.PowerTimerList import PowerTimerList
 from Components.Sources.StaticText import StaticText
-from Components.Sources.StaticText import StaticText
 from PowerTimer import PowerTimerEntry, AFTEREVENT
 from Screens.ChoiceBox import ChoiceBox
 from Screens.MessageBox import MessageBox
@@ -151,7 +150,7 @@ class PowerTimerEditList(Screen):
 
 		showCleanup = True
 		for x in self.list:
-			if (not x[0].disabled) and (x[1] == True):
+			if (not x[0].disabled) and (x[1] is True):
 				break
 		else:
 			showCleanup = False
