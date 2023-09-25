c1e328ce1d490dee417a4e759b8cd7c41c885e86
Twol <tonywhalley@t-online.de>
2023-09-25 14:21:04 +0200

    HdmiCec fix indent

--- lib/python/Components/HdmiCec.py
+++ lib/python/Components/HdmiCec.py
@@ -599,24 +599,24 @@ class HdmiCec:
 				self.sendStandbyMessages()
 
 	def sendStandbyMessages(self):
-			messages = []
-			if config.hdmicec.control_tv_standby.value:
-				if self.useStandby and not self.handlingStandbyFromTV:
-					messages.append("standby")
-				else:
-					messages.append("sourceinactive")
-					self.useStandby = True
+		messages = []
+		if config.hdmicec.control_tv_standby.value:
+			if self.useStandby and not self.handlingStandbyFromTV:
+				messages.append("standby")
 			else:
-				if config.hdmicec.report_active_source.value:
-					messages.append("sourceinactive")
-				if config.hdmicec.report_active_menu.value:
-					messages.append("menuinactive")
-			if messages:
-				self.sendQMessages(0, messages)
+				messages.append("sourceinactive")
+				self.useStandby = True
+		else:
+			if config.hdmicec.report_active_source.value:
+				messages.append("sourceinactive")
+			if config.hdmicec.report_active_menu.value:
+				messages.append("menuinactive")
+		if messages:
+			self.sendQMessages(0, messages)
 
-			if config.hdmicec.control_receiver_standby.value:
-				self.sendMessage(5, "keypoweroff")
-				self.sendMessage(5, "standby")
+		if config.hdmicec.control_receiver_standby.value:
+			self.sendMessage(5, "keypoweroff")
+			self.sendMessage(5, "standby")
 
 	def standby(self):			# Standby initiated from TV
 		if not Screens.Standby.inStandby:
