d6a2e7160fe3015d749605dd2be7a57c869e0556
Twol <tonywhalley@t-online.de>
2023-09-29 15:25:21 +0200

    [MediaScanner} PEP8

--- lib/python/Plugins/Extensions/MediaScanner/plugin.py
+++ lib/python/Plugins/Extensions/MediaScanner/plugin.py
@@ -1,12 +1,13 @@
 from os import access as osaccess, path as ospath, F_OK, R_OK
-
+from Components.Harddisk import harddiskmanager
 from Components.Scanner import scanDevice
 from Plugins.Plugin import PluginDescriptor
+from Screens.ChoiceBox import ChoiceBox
 from Screens.InfoBar import InfoBar
 
 
 def execute(option):
-#	print("[MediaScanner] execute", option)
+	# print("[MediaScanner] execute", option)
 	if option is None:
 		return
 
@@ -18,13 +19,11 @@ def mountpoint_chosen(option):
 	if option is None:
 		return
 
-	from Screens.ChoiceBox import ChoiceBox
-
 	# print("[MediaScanner][ mountpoint_chosen] scanning", option)
 	(description, mountpoint, session) = option
 	res = scanDevice(mountpoint)
 	list = [(r.description, r, res[r], session) for r in res]
-#	print("[MediaScanner][ mountpoint_chosen]  description=%s, mountpoint=%s res=%s list=%s" % (description, mountpoint, res, list))
+	# print("[MediaScanner][ mountpoint_chosen]  description=%s, mountpoint=%s res=%s list=%s" % (description, mountpoint, res, list))
 
 	if not list:
 		from Screens.MessageBox import MessageBox
@@ -42,7 +41,7 @@ def mountpoint_chosen(option):
 def scan(session):
 	from Screens.ChoiceBox import ChoiceBox
 	parts = [(r.tabbedDescription(), r.mountpoint, session) for r in harddiskmanager.getMountedPartitions(onlyhotplug=False) if osaccess(r.mountpoint, F_OK | R_OK)]
-#	print("[MediaScanner][scan] parts", parts)
+	# print("[MediaScanner][scan] parts", parts)
 	parts.append((_("Memory") + "\t/tmp", "/tmp", session))
 	session.openWithCallback(mountpoint_chosen, ChoiceBox, title=_("Please select medium to be scanned"), list=parts)
 
@@ -53,9 +52,6 @@ def main(session, **kwargs):
 
 def menuEntry(*args):
 	mountpoint_chosen(args)
-
-
-from Components.Harddisk import harddiskmanager
 
 
 def menuHook(menuid):
@@ -121,34 +117,34 @@ def filescan(**kwargs):
 	return [
 		Scanner(mimetypes=["video/mpeg", "video/MP2T", "video/x-msvideo", "video/mkv", "video/avi"],
 			paths_to_scan=[
-					ScanPath(path="", with_subdirs=False),
-					ScanPath(path="movie", with_subdirs=False),
-				],
+				ScanPath(path="", with_subdirs=False),
+				ScanPath(path="movie", with_subdirs=False),
+			],
 			name="Movie",
 			description=_("View Movies..."),
 			openfnc=movielist_open,
 		),
 		Scanner(mimetypes=["video/x-vcd"],
 			paths_to_scan=[
-					ScanPath(path="mpegav", with_subdirs=False),
-					ScanPath(path="MPEGAV", with_subdirs=False),
-				],
+				ScanPath(path="mpegav", with_subdirs=False),
+				ScanPath(path="MPEGAV", with_subdirs=False),
+			],
 			name="Video CD",
 			description=_("View Video CD..."),
 			openfnc=movielist_open,
 		),
 		Scanner(mimetypes=["audio/mpeg", "audio/x-wav", "application/ogg", "audio/x-flac"],
 			paths_to_scan=[
-					ScanPath(path="", with_subdirs=False),
-				],
+				ScanPath(path="", with_subdirs=False),
+			],
 			name="Music",
 			description=_("Play Music..."),
 			openfnc=movielist_open,
 		),
 		Scanner(mimetypes=["audio/x-cda"],
 			paths_to_scan=[
-					ScanPath(path="", with_subdirs=False),
-				],
+				ScanPath(path="", with_subdirs=False),
+			],
 			name="Audio-CD",
 			description=_("Play Audio-CD..."),
 			openfnc=movielist_open,
@@ -159,7 +155,7 @@ def filescan(**kwargs):
 def Plugins(**kwargs):
 	return [
 		PluginDescriptor(name=_("Media scanner"), description=_("Scan files..."), where=PluginDescriptor.WHERE_PLUGINMENU, needsRestart=True, fnc=main),
-#		PluginDescriptor(where = PluginDescriptor.WHERE_MENU, fnc=menuHook),
+		# PluginDescriptor(where = PluginDescriptor.WHERE_MENU, fnc=menuHook),
 		PluginDescriptor(name=_("Media scanner"), where=PluginDescriptor.WHERE_FILESCAN, needsRestart=False, fnc=filescan),
 		PluginDescriptor(where=PluginDescriptor.WHERE_SESSIONSTART, needsRestart=True, fnc=sessionstart),
 		PluginDescriptor(where=PluginDescriptor.WHERE_AUTOSTART, needsRestart=True, fnc=autostart)
