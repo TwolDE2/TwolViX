fb0847ec758f97c019bff2897e4dbe4988fa7f37
Twol <tonywhalley@t-online.de>
2023-09-30 12:06:05 +0200

    PEP8

--- lib/python/Plugins/Extensions/PicturePlayer/ui.py
+++ lib/python/Plugins/Extensions/PicturePlayer/ui.py
@@ -139,7 +139,6 @@ class picshow(Screen):
 
 class Pic_Setup(ConfigListScreen, Screen):
 
-
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		# for the skin: first try MediaPlayerSettings, then Setup, this allows individual skinning
