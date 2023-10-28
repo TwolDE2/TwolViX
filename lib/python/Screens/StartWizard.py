d63d76d8a2654d78d141ae9e5b8f2baa22ce0376
Twol <tonywhalley@t-online.de>
2023-10-28 11:09:31 +0200

    NetworkWizard - move to Screens from Plugins

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,23 +1,26 @@
 from Components.config import config, ConfigBoolean, configfile
 from Components.Pixmap import Pixmap
 from Screens.LanguageSelection import LanguageWizard  # noqa: F401
+from Screens.NetworkWizard import NetworkWizard
 from Screens.Rc import Rc
-from Screens.WizardLanguage import WizardLanguage
-from Screens.WizardUserInterfacePositioner import UserInterfacePositionerWizard
-from Screens.Wizard import wizardManager
 from Screens.VideoWizard import VideoWizard
 from Screens.VuWizard import VuWizard
+from Screens.WizardLanguage import WizardLanguage
+from Screens.Wizard import wizardManager
+from Screens.WizardUserInterfacePositioner import UserInterfacePositionerWizard
 from Tools.Directories import fileExists, fileHas
 
 config.misc.firstrun = ConfigBoolean(default=True)
 config.misc.languageselected = ConfigBoolean(default=True)
 config.misc.videowizardenabled = ConfigBoolean(default=True)
 config.misc.networkenabled = ConfigBoolean(default=False)
+config.misc.networkenabled_negative = ConfigBoolean(default=False)
 config.misc.Vuwizardenabled = ConfigBoolean(default=False)
 config.misc.restorewizardrun = ConfigBoolean(default=False)
 
 if fileExists("/usr/bin/kernel_auto.bin") and fileExists("/usr/bin/STARTUP.cpio.gz") and not fileHas("/proc/cmdline", "kexec=1") and config.misc.firstrun.value:
 	config.misc.Vuwizardenabled.value = True
+config.misc.networkenabled_negative = !config.misc.networkenabled	
 print("[StartWizard][import] import.......")
 
 
@@ -38,5 +41,6 @@ class StartWizard(WizardLanguage, Rc):
 # wizardManager.registerWizard(VideoWizard, config.misc.Vuwizardenabled.value, priority=2)
 wizardManager.registerWizard(VuWizard, config.misc.Vuwizardenabled.value, priority=3)
 wizardManager.registerWizard(VideoWizard, config.misc.videowizardenabled.value, priority=10)
+wizardManager.registerWizard(NetworkWizard, config.misc.videowizardenabled_negative.value, priority=15)
 wizardManager.registerWizard(UserInterfacePositionerWizard, config.misc.firstrun.value, priority=20)
 wizardManager.registerWizard(StartWizard, config.misc.firstrun.value, priority=25)
--- lib/python/Screens/VideoWizard.py
+++ lib/python/Screens/VideoWizard.py
@@ -47,7 +47,6 @@ class VideoWizard(WizardLanguage, Rc):
 		</screen>"""
 
 	def __init__(self, session):
-		# FIXME anyone knows how to use relative paths from the plugin's directory?
 		self.xmlfile = resolveFilename(SCOPE_SKIN, "videowizard.xml")
 		self.hw = iAV		# needed by VideoWizard.xml do not change
 		WizardLanguage.__init__(self, session, showSteps=False, showStepSlider=False)
