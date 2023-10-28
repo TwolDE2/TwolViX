db54f78ce66929345584b20a636b536baa784afe
Twol <tonywhalley@t-online.de>
2023-10-28 13:26:24 +0200

    [StartWizard] - setup NetworkWizard to run 1st time only

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -20,7 +20,6 @@ config.misc.restorewizardrun = ConfigBoolean(default=False)
 
 if fileExists("/usr/bin/kernel_auto.bin") and fileExists("/usr/bin/STARTUP.cpio.gz") and not fileHas("/proc/cmdline", "kexec=1") and config.misc.firstrun.value:
 	config.misc.Vuwizardenabled.value = True
-config.misc.networkenabled_negative = not config.misc.networkenabled
 print("[StartWizard][import] import.......")
 
 
@@ -41,6 +40,6 @@ class StartWizard(WizardLanguage, Rc):
 # wizardManager.registerWizard(VideoWizard, config.misc.Vuwizardenabled.value, priority=2)
 wizardManager.registerWizard(VuWizard, config.misc.Vuwizardenabled.value, priority=3)
 wizardManager.registerWizard(VideoWizard, config.misc.videowizardenabled.value, priority=10)
-wizardManager.registerWizard(NetworkWizard, config.misc.videowizardenabled_negative.value, priority=15)
+wizardManager.registerWizard(NetworkWizard, config.misc.firstrun.value, priority=15)
 wizardManager.registerWizard(UserInterfacePositionerWizard, config.misc.firstrun.value, priority=20)
 wizardManager.registerWizard(StartWizard, config.misc.firstrun.value, priority=25)
