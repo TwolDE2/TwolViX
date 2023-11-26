013e7962a187dc4e8590ec04f785f8b94a457da4
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2023-11-26 11:55:39 +0200

    [Fixed] Separator char for IPTV providers functionality

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -272,7 +272,7 @@ RESULT eStaticServiceMP3Info::getName(const eServiceReference &ref, std::string 
 			name = ref.path;
 	}
 	if (!name.empty()) {
-	 	std::vector<std::string> name_split = split(name, "|");
+	 	std::vector<std::string> name_split = split(name, "•");
 	 	name = name_split[0];
 	 }
 	return 0;
@@ -1214,7 +1214,7 @@ RESULT eServiceMP3::getName(std::string &name)
 		name = title;
 
 	if (!name.empty()) {
-	 	std::vector<std::string> name_split = split(name, "|");
+	 	std::vector<std::string> name_split = split(name, "•");
 	 	name = name_split[0];
 		if (name_split.size() > 1) {
 			m_prov = name_split[1];
