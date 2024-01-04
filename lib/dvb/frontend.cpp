69b57d041d5e4cc70a4f49339462f50665079169
Tony Whalley <tonywhalley@t-online.de>
2024-01-04 11:27:58 +0100

    [frontend] fix dB tuner name  VU+ Zero4K

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1361,7 +1361,7 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 	}
 	else if (!strcmp(m_description, "Vuplus DVB-S NIM(Si2166)")) // VU+ Zero4K
 	{
-		ret = snr;
+		ret = snr / 40;
 	}
 	else if (!strncmp(m_description, "Si216", 5)) // New models with SI tuners
 	{
