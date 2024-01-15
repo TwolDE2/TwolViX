8048378a0b92bcc177305eec5afa0fa669f667b0
Huevos <rowland@moralet.com>
2024-01-15 12:49:41 +0100

    [PackageManager] update location

--- lib/python/Plugins/SystemPlugins/ViX/plugin.py
+++ lib/python/Plugins/SystemPlugins/ViX/plugin.py
@@ -105,6 +105,17 @@ def SoftcamSetup(menuid):
 	return []
 
 
+def PackageManagerMenu(session, **kwargs):
+	from .PackageManager import PackageManager
+	session.open(PackageManager)
+
+
+def PackageManagerSetup(menuid):
+	if config.usage.setup_level.index > 1 and menuid == "softwareupdatemenu":
+		return [(_("Package Manager"), PackageManagerMenu, "packagemanager", 1005)]
+	return []
+
+
 def BackupManager(session):
 	from .BackupManager import VIXBackupManager
 	return VIXBackupManager(session)
@@ -189,9 +200,11 @@ def Plugins(**kwargs):
 			plist.append(PluginDescriptor(name=_("Vu+ ImageManager wizard"), where=PluginDescriptor.WHERE_WIZARD, needsRestart=False, fnc=(30, ImageManager)))
 		return plist
 
-	plist = [PluginDescriptor(where=PluginDescriptor.WHERE_MENU, needsRestart=False, fnc=startSetup),
-			PluginDescriptor(name=_("ViX Image Management"), where=PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=UpgradeMain),
-			PluginDescriptor(where=PluginDescriptor.WHERE_MENU, fnc=SoftcamSetup)]
+	plist = [
+		PluginDescriptor(where=PluginDescriptor.WHERE_MENU, needsRestart=False, fnc=startSetup),
+		PluginDescriptor(name=_("ViX Image Management"), where=PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=UpgradeMain),
+		PluginDescriptor(where=PluginDescriptor.WHERE_MENU, fnc=SoftcamSetup),
+		PluginDescriptor(where=PluginDescriptor.WHERE_MENU, fnc=PackageManagerSetup)]
 	if config.softcammanager.showinextensions.value:
 		plist.append(PluginDescriptor(name=_("Softcam manager"), where=PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=SoftcamMenu))
 	if config.scriptrunner.showinextensions.value:
