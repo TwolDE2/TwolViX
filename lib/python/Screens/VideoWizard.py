3f954029b211067cdc5bca07da5133f410c6078b
Tony Whalley <tonywhalley@t-online.de>
2024-01-29 14:50:19 +0100

    [VideoWizard] - don't initialise AVSwitch until later

--- lib/python/Screens/VideoWizard.py
+++ lib/python/Screens/VideoWizard.py
@@ -46,8 +46,6 @@ class VideoWizard(WizardLanguage, Rc):
 
 	def __init__(self, session):
 		self.xmlfile = resolveFilename(SCOPE_SKIN, "videowizard.xml")
-		from Components.AVSwitch import iAVSwitch as iAV
-		self.hw = iAV		# needed by VideoWizard.xml do not change
 		WizardLanguage.__init__(self, session, showSteps=False, showStepSlider=False)
 		Rc.__init__(self)
 		self["wizard"] = Pixmap()
