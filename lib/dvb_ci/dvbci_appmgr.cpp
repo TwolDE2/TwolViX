357c928166edc14c98ed90a25dd1e7cf6bc68c2b
Tony Whalley <tonywhalley@t-online.de>
2024-07-15 11:43:27 +0200

    [dvbci_appmgr] - try modded trap

--- lib/dvb_ci/dvbci_appmgr.cpp
+++ lib/dvb_ci/dvbci_appmgr.cpp
@@ -66,7 +66,7 @@ int eDVBCIApplicationManagerSession::receivedAPDU(const unsigned char *tag,const
 				/* emit */ eDVBCI_UI::getInstance()->m_messagepump.send(eDVBCIInterfaces::Message(eDVBCIInterfaces::Message::slotStateChanged, slot->getSlotID(), 2));
 				break;
 			}
-			catch( const exception& ex )
+			catch(std:exception ex)
 			{   
 			eDebug("[CI%d AM] catch in AM for utf-8 %d", slot->getSlotID(), ex.what());
  			} 			
