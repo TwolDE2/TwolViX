3026cea9676fca48a3538be8103e176f48604185
Tony Whalley <tonywhalley@t-online.de>
2024-10-03 15:20:31 +0200

    Revert "[DM900 ca test]"
    This reverts commit 58f1d0726da00f280ac2d086143e1f641da4eff5.

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -186,16 +186,10 @@ void eDVBAdapterLinux::scanDevices()
 		if (::access(filename, R_OK) < 0) break;
 		ePtr<eDVBDemux> demux;
 
-#ifdef DM900
-		++num_demux;
-#endif
-
 		demux = new eDVBDemux(m_nr, num_demux);
 		m_demux.push_back(demux);
 
-#ifndef DM900
 		++num_demux;
-#endif
 	}
 }
 
