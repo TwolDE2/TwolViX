251bf63f35e052940eced80727f9518ce92cda4b
AbuBaniaz <AbuBaniaz@users.noreply.github.com>
2024-10-28 17:10:07 +0000

    Whitespace cleanups By @atefganm

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -470,7 +470,7 @@ eServiceMP3::eServiceMP3(eServiceReference ref):
 	eDebug("[eServiceMP3] Init start %s", ref.toString().c_str());
 	if (!sref.empty())
 	{
-		eDebug("[eServiceMP3] Init start !sref.empty()");	
+		eDebug("[eServiceMP3] Init start !sref.empty()");
 		std::vector<eIPTVDBItem> &iptv_services = eDVBDB::getInstance()->iptv_services;
 		for(std::vector<eIPTVDBItem>::iterator it = iptv_services.begin(); it != iptv_services.end(); ++it)
 		{
@@ -479,7 +479,7 @@ eServiceMP3::eServiceMP3(eServiceReference ref):
 				m_currentAudioStream = it->ampeg_pid;
 				m_currentSubtitleStream = it->subtitle_pid;
 				m_cachedSubtitleStream = m_currentSubtitleStream;
-				eDebug("[eServiceMP3] Init start iptv_service use sref pid's A: %d; S: %d", m_currentAudioStream, it->subtitle_pid);				
+				eDebug("[eServiceMP3] Init start iptv_service use sref pid's A: %d; S: %d", m_currentAudioStream, it->subtitle_pid);
 			}
 		}
 	}
@@ -2691,7 +2691,7 @@ void eServiceMP3::pullSubtitle(GstBuffer *buffer)
 			{
 				uint8_t * data = map.data;
 				m_dvb_subtitle_parser->processBuffer(data, len, buf_pos / 1000000ULL);
-			} 
+			}
 			else if ( subType < stVOB )
 			{
 				int delay = eConfigManager::getConfigIntValue("config.subtitles.pango_subtitles_delay");
@@ -2746,7 +2746,7 @@ void eServiceMP3::pushDVBSubtitles()
 		}
 		else
 			return;
-		
+
 		decoder_ms = running_pts / 90;
 
 		// If subtitle is overdue or within 20ms the video timing then display it.
--- main/bsod.cpp
+++ main/bsod.cpp
@@ -200,7 +200,7 @@ void bsodFatal(const char *component)
 
 	std::string logtail;
 	int lines = 20;
-	
+
 	if (logp2)
 	{
 		unsigned int size = logs2;
@@ -212,7 +212,7 @@ void bsodFatal(const char *component)
 				if (!lines) {
 					logtail = std::string(r, logs2 - size);
 					break;
-				} 
+				}
 			}
 			else {
 				logtail = std::string(logp2, logs2);
@@ -232,7 +232,7 @@ void bsodFatal(const char *component)
 				if (!lines) {
 					logtail += std::string(r, logs1 - size);
 					break;
-				} 
+				}
 			}
 			else {
 				logtail += std::string(logp1, logs1);
