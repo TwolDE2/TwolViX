7e066934f5b30d1ac8da451f665da549efbff2c6
Huevos <rowland@moralet.com>
2024-11-29 21:23:46 +0100

    [InfoBarGenerics] avoid possible BSoD if self.servicelist is None
    https://www.world-of-satellite.com/showthread.php?66866-Crash-when-trying-to-post-the-EPG&p=541681#post541681

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1793,6 +1793,8 @@ class InfoBarEPG:
 		return services
 
 	def multiServiceEPG(self, type, showBouquet):
+		if not self.servicelist:
+			return
 		def openEPG(open, bouquet, bouquets):
 			if open:
 				bouquet = bouquet or self.servicelist.getRoot()
