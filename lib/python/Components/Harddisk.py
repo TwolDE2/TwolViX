0291740177a7928bd12ad3f7176713a9a982b3fc
TwolDE <tonywhalley@t-online.de>
2025-01-22 08:32:45 +0100

    [Harddisk] - clean code

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -816,8 +816,7 @@ class HarddiskManager:
 							description = self.getUserfriendlyDeviceName(partition, physicalDevice)
 							print(f"[Harddisk][enumerateBlockDevices]### Found partition '{partition}', description='{description}', device='{physicalDevice}' mountpoint='{self.getMountpoint(partition)}.")
 							if boxModel in ("dm900", "dm920") and partition == "mmcblk0p3" and self.getMountpoint(partition) == None:
-								self.mount_device = mountpoint = "/media/data/"
-								self.disk_path = "/dev/mmcblk0p3"
+								mountpoint = "/media/data/"
 								newFstab = fileReadLines("/etc/fstab")
 								newFstab.append("/dev/mmcblk0p3 /media/data ext4 rw, relatime,data=ordered 0 0")
 								fileWriteLines("/etc/fstab", newFstab)
