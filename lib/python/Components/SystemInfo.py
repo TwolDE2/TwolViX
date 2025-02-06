fdfc04efd03927e85f5574e600e4efe98dc7cf8f
Ev0-BH <Ev0_BhT@outlook.com>
2025-02-06 18:26:41 +0000

    [SystemInfo/BpDevice] Add SD card mount for Quad4K / Quad4K Pro

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -313,6 +313,6 @@ SystemInfo["supportPcmMultichannel"] = fileCheck("/proc/stb/audio/multichannel_p
 SystemInfo["canBackupEMC"] = MODEL in ("hd51", "h7") and ("disk.img", "{SystemInfo['MBbootdevice']}") or MODEL in ("osmio4k", "osmio4kplus", "osmini4k") and ("emmc.img", "{SystemInfo['MBbootdevice']}") or SystemInfo["HasHiSi"] and ("usb_update.bin", "none")
 SystemInfo["canMode12"] = MODEL in ("hd51", "h7") and ("brcm_cma=440M@328M brcm_cma=192M@768M", "brcm_cma=520M@248M brcm_cma=200M@768M")
 SystemInfo["HasH9SD"] = MODEL in ("h9", "i55plus") and pathExists("/dev/mmcblk0p1")
-SystemInfo["HasSDnomount"] = MODEL in ("h9", "i55plus") and (False, "none") or MODEL in ("multibox", "h9combo", "h9combose", "h9twin", "h9se", "pulse4kmini", "hd61", "pulse4k", "h11") and (True, "mmcblk0")
+SystemInfo["HasSDnomount"] = MODEL in ("h9", "i55plus") and (False, "none") or MODEL in ("h9combo", "h9combose", "h9se", "h9twin", "h9twinse", "h11", "multibox", "multiboxpro", "pulse4k", "pulse4kmini", "gb7252") and (True, "mmcblk0")
 SystemInfo["haveboxmode"] = fileCheck("/proc/stb/info/boxmode")
 print("[SystemInfo] SystemInfo data initialised.")
