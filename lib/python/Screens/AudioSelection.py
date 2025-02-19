b055342161809084d99b4000762c237df6622802
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2025-02-19 18:53:22 +0200

    [Fixed] Broken audio/subs saving for iptv

--- lib/python/Screens/AudioSelection.py
+++ lib/python/Screens/AudioSelection.py
@@ -502,7 +502,7 @@ class AudioSelection(ConfigListScreen, Screen):
 		track = int(audio)
 		if isinstance(track, int):
 			service = self.session.nav.getCurrentService()
-			ref = self.session.nav.getCurrentServiceRef()
+			ref = self.session.nav.getCurrentServiceReferenceOriginal()
 			ref = ref and eServiceReference(ref)
 			if service.audioTracks().getNumberOfTracks() > track:
 				self.audioTracks.selectTrack(track)
@@ -611,7 +611,7 @@ class AudioSelection(ConfigListScreen, Screen):
 	def keyOk(self):
 		if self.focus == FOCUS_STREAMS and self["streams"].list:
 			cur = self["streams"].getCurrent()
-			ref = self.session.nav.getCurrentServiceRef()
+			ref = self.session.nav.getCurrentServiceReferenceOriginal()
 			ref = ref and eServiceReference(ref)
 			if self.settings.menupage.value == PAGE_AUDIO and cur[0] is not None:
 				self.changeAudio(cur[0])
