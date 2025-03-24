a0d77df88a6aedda15ed4293c02dc47c35755ffe
TwolDE <tonywhalley@t-online.de>
2025-03-24 14:18:33 +0100

    [EventInfo] null print debug

--- lib/python/Components/Sources/EventInfo.py
+++ lib/python/Components/Sources/EventInfo.py
@@ -155,7 +155,7 @@ class EventInfo(PerServiceBase, Source):
 
 	def gotEvent(self, what, from_timer=False):
 		self.timer.stop()
-		print("[EventInfo] gotEvent, type:", ("'now'" if self.now_or_next == self.NOW else "'next'") + ",", "what:", "'%s'" % str({iPlayableService.evStart: "evStart", iPlayableService.evUpdatedInfo: "evUpdatedInfo", iPlayableService.evUpdatedEventInfo: "evUpdatedEventInfo", iPlayableService.evEnd: "evEnd"}.get(what, "Unknown")) + ",", "is timed repeat:", str(from_timer))
+		#  print("[EventInfo] gotEvent, type:", ("'now'" if self.now_or_next == self.NOW else "'next'") + ",", "what:", "'%s'" % str({iPlayableService.evStart: "evStart", iPlayableService.evUpdatedInfo: "evUpdatedInfo", iPlayableService.evUpdatedEventInfo: "evUpdatedEventInfo", iPlayableService.evEnd: "evEnd"}.get(what, "Unknown")) + ",", "is timed repeat:", str(from_timer))
 		if what == iPlayableService.evEnd and not self.__service:
 			self.changed((self.CHANGED_CLEAR,))
 		else:
@@ -163,9 +163,9 @@ class EventInfo(PerServiceBase, Source):
 		# if evUpdatedEventInfo arrives before the event starts the fields will not change, so add an additional future timed event to make sure it does update.
 		if not from_timer and what in (iPlayableService.evUpdatedInfo, iPlayableService.evUpdatedEventInfo):
 			self.timer.startLongTimer(wait := 300 - int(time() % 300) + 5)  # repeat event at nearest 5 minutes ahead + 5 seconds
-			print("[EventInfo] gotEvent, timer is set to repeat event in %s seconds" % wait)
+			#  print("[EventInfo] gotEvent, timer is set to repeat event in %s seconds" % wait)
 		if self.now_or_next == self.NOW and what in (iPlayableService.evUpdatedInfo, iPlayableService.evUpdatedEventInfo):
-			print("[EventInfo] current event:", str(self.event and hasattr(self.event, "getEventName") and callable(self.event.getEventName) and self.event.getEventName() or self.event))
+			#  print("[EventInfo] current event:", str(self.event and hasattr(self.event, "getEventName") and callable(self.event.getEventName) and self.event.getEventName() or self.event))
 
 	def destroy(self):
 		PerServiceBase.destroy(self)
