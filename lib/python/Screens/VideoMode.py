e59f684941c2f29066781cda0f23bcc043dd11c7
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2025-04-04 22:34:16 +0300

    [Fixed] Some typos [Remove] Duo4K from passthrough fix

--- lib/python/Screens/VideoMode.py
+++ lib/python/Screens/VideoMode.py
@@ -66,9 +66,9 @@ class VideoSetup(Setup):
 			self.list.append(getConfigListEntry(_("Allow 10bit"), config.av.allow_10bit, _("Enable or disable the 10 Bit Color Mode")))
 		if SystemInfo["CanDownmixAC3"]:
 			self.list.append(getConfigListEntry(_("AC3 downmix"), config.av.downmix_ac3, _("Choose whether multi channel ac3 sound tracks should be downmixed to stereo.")))
-			if SystemInfo["Passthrough_fix_needed"] and config.av.downmix_ac3.value == "passthrough":
-				self.list.append(getConfigListEntry(_("Passthrough audio handling delay short"), config.av.passthrought_fix_short, _("Used when there is AC3 passthrough and specify the delay when switching service.")))
-				self.list.append(getConfigListEntry(_("Passthrough audio handling delay lon"), config.av.passthrought_fix_long, _("Used when there is AC3 passthrough and specify the delay when switching service.")))
+			if SystemInfo["Vu_EAC3_fix"] and config.av.downmix_ac3.value == "passthrough":
+				self.list.append(getConfigListEntry(_("Passthrough audio handling delay AC3"), config.av.passthrough_fix_short, _("Used to specify delay when switching between services and AC3 passthrough is enabled.")))
+				self.list.append(getConfigListEntry(_("Passthrough audio handling delay AC3+"), config.av.passthrough_fix_long, _("Used to specify delay when switching between services and AC3+/Atmos passthrough is enabled.")))
 		if SystemInfo["CanDownmixDTS"]:
 			self.list.append(getConfigListEntry(_("DTS downmix"), config.av.downmix_dts, _("Choose whether multi channel DTS sound tracks should be downmixed to stereo.")))
 		if SystemInfo["CanDownmixAAC"]:
