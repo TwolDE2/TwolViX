da2c1537e9515519511c0d55c59489d9020f25f6
jbleyel <jbleyel@gmx.net>
2025-04-16 22:36:32 +0200

    [eDVBServicePMTHandler] - add pvrDescramble support
    * Vu+ Uno4K, Solo4K,  Zero4K, Uno4KSE (OE-A Commit e605ca7)

--- lib/dvb/pmt.cpp
+++ lib/dvb/pmt.cpp
@@ -191,6 +191,8 @@ void eDVBServicePMTHandler::PMTready(int error)
 			else
 				eDebug("[PMT][eDVBServicePMTHandler] cannot call buildCAPMT");
 		}
+		if (m_service_type == pvrDescramble)
+			serviceEvent(eventStartPvrDescramble);
 	}
 }
 
