00fd4908411c4f32833be24e403205553203d739
TwolDE <tonywhalley@t-online.de>
2025-06-26 09:54:19 +0200

    [InputDeviceSetup] add H17, pulse
    sort imports

--- lib/python/Screens/InputDeviceSetup.py
+++ lib/python/Screens/InputDeviceSetup.py
@@ -244,7 +244,8 @@ class RemoteControlType(ConfigListScreen, Screen):
 		("25", _("Zgemma H9/I55Plus old Model")),
 		("26", _("Protek 4K UHD/HD61")),
 		("27", _("HD60")),
-		("28", _("H7/H9/H9COMBO/H10 new Model"))
+		("28", _("I55SE/H7/H17/H9/H9SE/H9COMBO/H9COMBOSE/H10/H11 new Model")
+		("30", _("PULSe 4K/4K Mini"))
 	]
 
 	defaultRcList = [
@@ -284,9 +285,13 @@ class RemoteControlType(ConfigListScreen, Screen):
 		("hd61", 26),
 		("hd60", 27),
 		("h7", 28),  # new model
+		("h17", 28),
 		("h9", 28),  # new model
 		("h9combo", 28),
-		("h10", 28)
+		("h10", 28),
+		("h11", 28),
+		("pulse4k", 30),
+		("pulse4kmini", 30)
 	]
 
 	def __init__(self, session):
