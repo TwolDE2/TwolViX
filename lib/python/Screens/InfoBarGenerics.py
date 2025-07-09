74e849c6682b5def8f5b28d20986055395d3d54f
jbleyel <jbleyel@gmx.net>
2025-07-09 12:17:42 +0200

    Force pickle protocol version 5 because the default protocol of python 3.13 is version 6

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -7,7 +7,7 @@ from re import match
 from sys import maxsize
 from time import time, localtime, strftime
 
-from pickle import load as pickle_load, dump as pickle_dump, HIGHEST_PROTOCOL as pickle_HIGHEST_PROTOCOL
+from pickle import load as pickle_load, dump as pickle_dump
 from enigma import eTimer, eServiceCenter, eDVBServicePMTHandler, iServiceInformation, iPlayableService, iRecordableService, eServiceReference, eEPGCache, eActionMap, getDesktop, eDVBDB
 from keyids import KEYIDS
 # from keyids import KEYFLAGS, KEYIDNAMES  # used by print debug
@@ -95,7 +95,7 @@ class ResumePoints():
 
 	def saveResumePoints(self):
 		with open(self.resumePointFile, "wb") as f:
-			pickle_dump(self.resumePointCache, f, pickle_HIGHEST_PROTOCOL)
+			pickle_dump(self.resumePointCache, f, protocol=5))
 
 	def delResumePoint(self, ref):
 		if (sref := ref.toString()) in self.resumePointCache:
