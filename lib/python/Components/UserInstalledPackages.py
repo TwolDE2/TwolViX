9d586bae9571a0b20e5212ebe48db38e789a2346
TwolDE <tonywhalley@t-online.de>
2025-10-13 13:11:28 +0200

    [User-Installed-Packages] update exclusions

--- lib/python/Components/UserInstalledPackages.py
+++ lib/python/Components/UserInstalledPackages.py
@@ -2,7 +2,7 @@ class UserInstalledPackages:
 	def __init__(self):
 		self.opkg_package_list = "/var/lib/opkg/status"
 		self.autopackages = ("opkg", "openvix-base", "run-postinsts")  # Auto installed packages not marked "Auto-Installed: yes" in "/var/lib/opkg/status"
-
+		self.autoViXpackages = ("mc", "mc-helpers", "mc-shell", "nano", "packagegroup-base-nfs", "packagegroup-base-smbfs-client", "packagegroup-base-smbfs-server")  # Auto installed packages not marked "Auto-Installed from recipes-distros in build: yes" in "/var/lib/opkg/status"
 	def run(self, callback):
 		packages_out = []
 		for package in [x for x in open(self.opkg_package_list).read().split("\n\n") if not "Auto-Installed: yes" in x]:
@@ -13,6 +13,6 @@ class UserInstalledPackages:
 					p_name = line.replace("Package: ", "").strip()
 					break
 			print(f"[UserInstalledPackages] - pname:{p_name}")
-			if p_name and p_name not in self.autopackages and "locale" not in p_name:
+			if p_name and p_name not in self.autopackages and p_name not in self.autoViXpackages and "locale" not in p_name:
 				packages_out.append(p_name)
 		callback(packages_out)
