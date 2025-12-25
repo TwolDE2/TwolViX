b24f03f35a25af6d00327c814be336437e97704f
DimitarCC <dimitar_tsenev@outlook.com>
2025-12-25 16:44:26 +0200

    [Added] Scale of picons with keep aspect ratio and center horizontal and vertical

--- lib/python/Components/Renderer/PiconBg.py
+++ lib/python/Components/Renderer/PiconBg.py
@@ -1,4 +1,4 @@
-from enigma import ePixmap
+from enigma import ePixmap, BT_SCALE, BT_KEEP_ASPECT_RATIO, BT_HALIGN_CENTER, BT_VALIGN_CENTER
 
 from Components.config import config
 from Components.Renderer.Renderer import Renderer
@@ -22,7 +22,7 @@ class PiconBg(Renderer):
 				pngname = resolveFilename(SCOPE_CURRENT_SKIN, "piconbg/" + config.usage.show_picon_bkgrn.value + ".png")
 				if self.pngname != pngname:
 					if pngname:
-						self.instance.setScale(1)
+						self.instance.setPixmapScaleFlags(BT_SCALE | BT_KEEP_ASPECT_RATIO | BT_HALIGN_CENTER | BT_VALIGN_CENTER)
 						self.instance.setPixmapFromFile(pngname)
 						self.instance.show()
 					else:
