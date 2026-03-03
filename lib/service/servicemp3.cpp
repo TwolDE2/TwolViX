6cd0e313d279d30f20b12e222f4327519a050683
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2026-03-03 13:34:50 +0200

    [Fixed] A typo in ServiceMP3

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -820,7 +820,7 @@ void eServiceMP3::forceAudioReset()
 		int currAudioIndex = getCurrentTrack();
 		selectAudioStream(currAudioIndex, true);
 	}
-	}
+
 	m_clear_buffers = true;
 	clearBuffers();
 }
