e85dbb37951330e086d349d4526942af6015ada4
TwolDE2 <tonywhalley@web.de>
2026-03-09 09:51:59 +0000

    PEP8 double aggressive E225 ~ E228 and E231

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -337,7 +337,7 @@ avSwitch = iAVSwitch  # Not used by OpenViX. For compatibility with OpenATV. Use
 
 def InitAVSwitch():
 	if SystemInfo["Vu_EAC3_fix"]:
-		delay_choices = [(i, ngettext("%d milisecond", "%d miliseconds", i) % i) for i in list(range(0,3000,100))]  # noqa: F821
+		delay_choices = [(i, ngettext("%d milisecond", "%d miliseconds", i) % i) for i in list(range(0, 3000, 100))]  # noqa: F821
 		config.av.passthrough_fix_short = ConfigSelection(choices=delay_choices, default=100)
 	config.av.yuvenabled = ConfigYesNo(default=True)
 	colorformat_choices = {
