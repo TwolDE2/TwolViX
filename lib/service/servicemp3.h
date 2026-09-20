833cd2bffda2b525155f1815a36c970c286568ea
DimitarCC <dimitar_tsenev@outlook.com>
2026-09-20 09:12:57 +0300

    Revert "[service] Improve subtitle handling by disabling sync-streams on input-selector to prevent flushing seek stalls"
    This reverts commit 7d925facc293f2f5ab9af9c487db1f12cca0a09c.

--- lib/service/servicemp3.h
+++ lib/service/servicemp3.h
@@ -355,7 +355,6 @@ private:
 	void HandleTocEntry(GstMessage *msg);
 	static gint match_sinktype(const GValue *velement, const gchar *type);
 	static void handleElementAdded(GstBin *bin, GstElement *element, gpointer user_data);
-	void disableAllInputSelectorSyncStreams();
 	void disconnectAsyncSignalHandlers();
 
 	struct subtitle_page_t
